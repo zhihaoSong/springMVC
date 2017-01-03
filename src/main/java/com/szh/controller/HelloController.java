51b140fb23a754c2ca484f9f53f8e190a4133aed
zhihao.song <1551151530@qq.com>
2017-01-03 20:29:11 +0800

    first commit

---
+++ src/main/java/com/szh/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.szh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "hello";
+    }
+
+    @RequestMapping("sss")
+    public String printWelcomeSB(ModelMap model) {
+        model.addAttribute("message", "Hello worldsss!");
+        return "hello";
+    }
+
+}
